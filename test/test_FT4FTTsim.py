da90a76f69d72d6a39c39cd7906da46e2ddee34e
David Gessner <davidges@gmail.com>
2013-10-13 20:55:57 +0200

    Add more unit tests for class Link

--- test/test_FT4FTTsim.py
+++ test/test_FT4FTTsim.py
@@ -11,5 +11,13 @@ class TestLink(unittest.TestCase):
     def test_get_end_point(self):
         self.assertEqual(self.link.get_end_point(), self.destination)
 
+    def test_link_has_no_message_when_created(self):
+        self.assertFalse(self.link.has_message())
+
+    def test_put_message_then_has_message(self):
+        m = Message(self.source, [self.destination], "test message")
+        self.link.put_message(m)
+        self.assertEqual(self.link.get_message(), m)
+
 if __name__ == '__main__':
     unittest.main()
