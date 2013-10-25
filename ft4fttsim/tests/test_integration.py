356528439f1ff58004b3bed75b3b816c6cc86b9c
David Gessner <davidges@gmail.com>
2013-10-25 06:25:36 +0200

    Revise some tests

--- ft4fttsim/tests/test_integration.py
+++ ft4fttsim/tests/test_integration.py
@@ -1,6 +1,7 @@
 # author: David Gessner <davidges@gmail.com>
 
 import unittest
+from mock import sentinel, Mock
 from ft4fttsim.ethernet import Ethernet
 from ft4fttsim.networking import *
 
@@ -11,11 +12,9 @@ class TestLinkIntegration(unittest.TestCase):
         self.link = Link(10, 0)
 
     def test_get_message__after_put_message__returns_same_message(self):
-        source = NetworkDevice("source")
-        source.connect_outlink(self.link)
         destination = NetworkDevice("destination")
         destination.connect_inlink(self.link)
-        m = Message(source, [destination],
+        m = Message(sentinel.dummy_source, [destination],
             Ethernet.MAX_FRAME_SIZE_BYTES, "test message")
         self.link.put_message(m)
         self.assertEqual(self.link.get_message(), m)
@@ -91,59 +90,60 @@ class TestNetworkDeviceIntegration(unittest.TestCase):
         self.assertEqual(self.device.get_inlinks(), inlinks)
 
     def test_read_inlinks__put_message_on_1_inlink__returns_message(self):
-        source = NetworkDevice("source")
         link = Link(10, 0)
         self.device.connect_inlink(link)
-        test_message = Message(source, [self.device],
+        test_message = Message(sentinel.dummy_source, [self.device],
             Ethernet.MAX_FRAME_SIZE_BYTES, "test message")
         link.put_message(test_message)
         received_messages = self.device.read_inlinks()
         self.assertEqual(received_messages, [test_message])
 
     def test_read_inlinks__1_empty_inlink__returns_empty_list(self):
-        source = NetworkDevice("source")
         inlink = Link(10, 0)
         self.device.connect_inlink(inlink)
         received_messages = self.device.read_inlinks()
         self.assertEqual(received_messages, [])
 
+
+class TestNetworkDeviceWith2Inlinks(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set up the following configuration:
+
+                      +--------+
+         inlink1 ---> |        |
+                      | device |
+         inlink2 ---> |        |
+                      +--------+
+        """
+        self.device = NetworkDevice("test device")
+        self.inlink1 = Link(10, 0)
+        self.inlink2 = Link(10, 0)
+        self.device.connect_inlink(self.inlink1)
+        self.device.connect_inlink(self.inlink2)
+
     def test_read_inlinks__2_empty_inlinks__returns_empty_list(self):
-        source = NetworkDevice("source")
-        inlink1 = Link(10, 0)
-        inlink2 = Link(10, 0)
-        self.device.connect_inlink(inlink1)
-        self.device.connect_inlink(inlink2)
         received_messages = self.device.read_inlinks()
         self.assertEqual(received_messages, [])
 
     def test_read_inlinks__put_message_on_2_inlinks__returns_messages(self):
-        source = NetworkDevice("source")
-        inlink1 = Link(10, 0)
-        inlink2 = Link(10, 0)
-        self.device.connect_inlink(inlink1)
-        self.device.connect_inlink(inlink2)
-        test_message = Message(source, [self.device],
+        test_message = Message(sentinel.dummy_source, [self.device],
             Ethernet.MAX_FRAME_SIZE_BYTES, "test message")
-        inlink1.put_message(test_message)
-        inlink2.put_message(test_message)
+        self.inlink1.put_message(test_message)
+        self.inlink2.put_message(test_message)
         received_messages = self.device.read_inlinks()
         self.assertEqual(received_messages, [test_message, test_message])
 
     def test_read_inlinks__put_diff_msg_on_2_inlinks__returns_messages(self):
-        source = NetworkDevice("source")
-        inlink1 = Link(10, 0)
-        inlink2 = Link(10, 0)
-        self.device.connect_inlink(inlink1)
-        self.device.connect_inlink(inlink2)
-        test_message1 = Message(source, [self.device],
+        test_message1 = Message(sentinel.dummy_source, [self.device],
             Ethernet.MAX_FRAME_SIZE_BYTES, "test message1")
-        test_message2 = Message(source, [self.device],
+        test_message2 = Message(sentinel.dummy_source, [self.device],
             Ethernet.MAX_FRAME_SIZE_BYTES, "test message2")
-        inlink1.put_message(test_message1)
-        inlink2.put_message(test_message2)
+        self.inlink1.put_message(test_message1)
+        self.inlink2.put_message(test_message2)
         received_messages = self.device.read_inlinks()
         self.assertEqual(received_messages, [test_message1, test_message2])
-
 
 if __name__ == '__main__':
     unittest.main()
