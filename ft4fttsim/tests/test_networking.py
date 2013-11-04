36ed06593344796a8b4ea029a316663597cfdd34
David Gessner <davidges@gmail.com>
2013-11-04 09:15:33 +0100

    Move Link related tests to a new file

--- ft4fttsim/tests/test_networking.py
+++ ft4fttsim/tests/test_networking.py
@@ -7,47 +7,6 @@ from ft4fttsim.exceptions import FT4FTTSimException
 import pytest
 
 
-@pytest.mark.parametrize("megabits,propagation_delay", [
-    # negative propagation delay raises exception
-    (10, -1),
-    # zero megabits per second raises exception
-    (0, 1),
-    # negative megabits per second raises exception
-    (-1, 1),
-    # negative megabits per second and negative propagation delay raises
-    # exception
-    (-5, -5),
-])
-def test_link_constructor_raises_exception(env, megabits, propagation_delay):
-    with pytest.raises(FT4FTTSimException):
-        Link(env, megabits, propagation_delay)
-
-
-@pytest.mark.parametrize("megabits,propagation_delay", [
-    # zero propagation delay does not raise an exception
-    (10, 0),
-
-    # example constructor parameters that should not raise an exception
-    (1, 1),
-    (5, 5),
-    (100000, 100000),
-    (0.00001, 0.00001),
-    (0.00001, 100000),
-    (100000, 0.00001),
-])
-def test_link_constructor_does_not_raise_exception(
-        env, megabits, propagation_delay):
-    try:
-        Link(env, megabits, propagation_delay)
-    except:
-        assert False, "Link constructor should not raise exception."
-
-
-def test_get_end_point__link_created__end_point_is_None(env):
-    link = Link(env, 10, 0)
-    assert link.end_point == None
-
-
 MINIMUM_ETHERNET_FRAME_SIZE = 64
 MAXIMUM_ETHERNET_FRAME_SIZE = 1518
 
