3a9d10476301e5b990af7a732ec7911a768a2066
David Gessner <davidges@gmail.com>
2013-11-05 12:29:08 +0100

    Rename some variables

--- ft4fttsim/tests/test_link.py
+++ ft4fttsim/tests/test_link.py
@@ -5,7 +5,7 @@ from ft4fttsim.networking import Link
 from ft4fttsim.exceptions import FT4FTTSimException
 
 
-@pytest.mark.parametrize("megabits,propagation_delay", [
+@pytest.mark.parametrize("Mbps,propagation_delay", [
     # negative propagation delay raises exception
     (10, -1),
     # zero megabits per second raises exception
@@ -16,12 +16,12 @@ from ft4fttsim.exceptions import FT4FTTSimException
     # exception
     (-5, -5),
 ])
-def test_link_constructor_raises_exception(env, megabits, propagation_delay):
+def test_link_constructor_raises_exception(env, Mbps, propagation_delay):
     with pytest.raises(FT4FTTSimException):
-        Link(env, megabits, propagation_delay)
+        Link(env, Mbps, propagation_delay)
 
 
-@pytest.mark.parametrize("megabits,propagation_delay", [
+@pytest.mark.parametrize("Mbps,propagation_delay", [
     # zero propagation delay does not raise an exception
     (10, 0),
 
@@ -34,9 +34,9 @@ def test_link_constructor_raises_exception(env, megabits, propagation_delay):
     (100000, 0.00001),
 ])
 def test_link_constructor_does_not_raise_exception(
-        env, megabits, propagation_delay):
+        env, Mbps, propagation_delay):
     try:
-        Link(env, megabits, propagation_delay)
+        Link(env, Mbps, propagation_delay)
     except:
         assert False, "Link constructor should not raise exception."
 
@@ -47,11 +47,11 @@ def test_link_created__receiver_is_None(env):
 
 
 @pytest.mark.parametrize(
-    "megabits,num_bytes,expected_transmission_time",
+    "Mbps,num_bytes,expected_transmission_time",
     [(1, 1526, 12208), (10, 1526, 1220.8), (100, 1526, 122.08),
     (1000, 1526, 12.208), (100, 0, 0)]
 )
-def test_link__transmission_time_us(env, megabits, num_bytes,
+def test_link__transmission_time_us(env, Mbps, num_bytes,
         expected_transmission_time):
-    link = Link(env, megabits, 0)
+    link = Link(env, Mbps, 0)
     assert link.transmission_time_us(num_bytes) == expected_transmission_time
