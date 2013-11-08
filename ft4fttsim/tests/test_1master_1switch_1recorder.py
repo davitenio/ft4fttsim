0d2e199efd4f5607d99a75234eeb2a864693284f
David Gessner <davidges@gmail.com>
2013-11-08 15:50:31 +0100

    Rewrite master, switch, recorder tests using new fixtures

--- ft4fttsim/tests/test_1master_1switch_1recorder.py
+++ ft4fttsim/tests/test_1master_1switch_1recorder.py
@@ -1,46 +1,69 @@
 # author: David Gessner <davidges@gmail.com>
+"""
+Perform tests under the following network:
+
++--------+      +--------+      +----------+
+| master | ---> | switch | ---> | recorder |
++--------+      +--------+      +----------+
+"""
 
 import pytest
-from ft4fttsim.networking import *
-from ft4fttsim.masterslave import *
-from ft4fttsim.ethernet import *
+from ft4fttsim.tests.fixturehelper import make_link
+from ft4fttsim.tests.fixturehelper import LINK_CONFIGS
 
 
-class Test1Master1Switch1Recorder:
+@pytest.fixture(params=LINK_CONFIGS)
+def link1(env, request):
+    config = request.param
+    new_link = make_link(config, env)
+    return new_link
 
-    @pytest.fixture(autouse=True,
-        params=range(5))
-    def set_up_network(self, env, request):
-        """
-        Set up the following network:
 
-        +--------+      +--------+      +----------+
-        | master | ---> | switch | ---> | recorder |
-        +--------+      +--------+      +----------+
-        """
-        self.env = env
-        self.num_TMs_per_EC = request.param
-        self.switch = Switch(self.env, "test switch")
-        self.recorder = MessageRecordingDevice(self.env, "recorder")
-        # configured elementary cycle duration in microseconds
-        self.EC_duration_us = 10 ** 9
-        self.master = Master(self.env, "test master", [self.recorder],
-            self.EC_duration_us, self.num_TMs_per_EC)
-        link_master_switch = Link(self.env, 100, 0)
-        link_switch_recorder = Link(self.env, 100, 0)
-        self.master.connect_outlink(link_master_switch)
-        self.switch.connect_inlink(link_master_switch)
-        self.switch.connect_outlink(link_switch_recorder)
-        self.recorder.connect_inlink(link_switch_recorder)
+@pytest.fixture(params=LINK_CONFIGS)
+def link2(env, request):
+    config = request.param
+    new_link = make_link(config, env)
+    return new_link
 
-    @pytest.mark.parametrize("num_ECs", range(1, 4))
-    def test_num_ECs_simulated__record_correct_number_of_messages(
-            self, num_ECs):
-        """
-        Test that if we simulate for the duration of num_ECs elementary
-        cycles, the recorder records num_ECs * self.num_TMs_per_EC
-        messages.
-        """
-        self.env.run(until=num_ECs * self.EC_duration_us)
-        received_messages = self.recorder.recorded_messages
-        assert len(received_messages) == num_ECs * self.num_TMs_per_EC
+
+@pytest.fixture
+def recorder(env, link2):
+    from ft4fttsim.networking import MessageRecordingDevice
+    recorder = MessageRecordingDevice(env, "recorder")
+    recorder.connect_inlink(link2)
+    return recorder
+
+
+@pytest.fixture(params=range(4))
+def master(request, env, link1, recorder):
+    from ft4fttsim.masterslave import Master
+    # number of trigger messages per elementary cycle
+    num_TMs_per_EC = request.param
+    # configured elementary cycle duration in microseconds
+    EC_duration_us = 10 ** 9
+    new_master = Master(env, "master", [recorder], EC_duration_us,
+        num_TMs_per_EC)
+    new_master.connect_outlink(link1)
+    return new_master
+
+
+@pytest.fixture
+def master_switch_recorder(env, master, switch, recorder,
+        link1, link2):
+    switch.connect_inlink(link1)
+    switch.connect_outlink(link2)
+    return master, switch, recorder
+
+
+@pytest.mark.parametrize("num_ECs", range(1, 4))
+def test_num_ECs_simulated__record_correct_number_of_messages(
+        env, master_switch_recorder, num_ECs):
+    """
+    Test that the recorder records master.num_TMs_per_EC trigger messages in
+    each elementary cycle.
+    """
+    master = master_switch_recorder[0]
+    recorder = master_switch_recorder[2]
+    env.run(until=num_ECs * master.EC_duration_us)
+    received_messages = recorder.recorded_messages
+    assert len(received_messages) == num_ECs * master.num_TMs_per_EC
