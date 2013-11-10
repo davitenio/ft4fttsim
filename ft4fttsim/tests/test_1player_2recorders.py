fec55385012a2ceb2f3ddafbac32af1b855dd85f
David Gessner <davidges@gmail.com>
2013-11-10 14:21:10 +0100

    Rewrite tests for 1 player and 2 recorders using pytest

--- ft4fttsim/tests/test_1player_2recorders.py
+++ ft4fttsim/tests/test_1player_2recorders.py
@@ -1,75 +1,98 @@
 # author: David Gessner <davidges@gmail.com>
+"""
+Execute tests under the following network:
 
-import unittest
-from ft4fttsim.networking import *
-from ft4fttsim.masterslave import *
-from ft4fttsim.ethernet import *
++--------+  link1    +-----------+
+|        | --------> | recorder1 |
+|        |           +-----------+
+| player |
+|        |  link2    +-----------+
+|        | --------> | recorder2 |
++--------+           +-----------+
 
+"""
 
-class Test1Player2Recorder(unittest.TestCase):
-
-    def setUp(self):
-        """
-        Set up the following network:
-
-        +--------+      +-----------+
-        |        | ---> | recorder1 |
-        |        |      +-----------+
-        | player |
-        |        |      +-----------+
-        |        | ---> | recorder2 |
-        +--------+      +-----------+
-        """
-        self.env = simpy.Environment()
-        self.player = MessagePlaybackDevice(self.env, "player")
-        self.recorder1 = MessageRecordingDevice(self.env, "recorder1")
-        self.recorder2 = MessageRecordingDevice(self.env, "recorder2")
-        self.link_Mbps = 100
-        self.link_propagation_delay_us = 3
-        link_player_recorder1 = Link(self.env, self.link_Mbps,
-            self.link_propagation_delay_us)
-        self.player.connect_outlink(link_player_recorder1)
-        self.recorder1.connect_inlink(link_player_recorder1)
-        link_player_recorder2 = Link(self.env, self.link_Mbps,
-            self.link_propagation_delay_us)
-        self.player.connect_outlink(link_player_recorder2)
-        self.recorder2.connect_inlink(link_player_recorder2)
+import pytest
+from ft4fttsim.tests.fixturehelper import make_link
 
 
-class TestOneMessagePerOutlink(Test1Player2Recorder):
-
-    def setUp(self):
-        """
-        Set up player sending a separate message at a different instant of time
-        to each recorder.
-        """
-        Test1Player2Recorder.setUp(self)
-        tx_start_time1, tx_start_time2 = range(2)
-        self.messages_to_transmit1 = [Message(self.env, self.player,
-            self.recorder1, Ethernet.MAX_FRAME_SIZE_BYTES,
-            "message for recorder1")]
-        self.messages_to_transmit2 = [Message(self.env, self.player,
-            self.recorder2, Ethernet.MAX_FRAME_SIZE_BYTES,
-            "message for recorder2")]
-        outlink1 = self.player.outlinks[0]
-        outlink2 = self.player.outlinks[1]
-        list_of_commands = {}
-        transmission_command1 = {outlink1: self.messages_to_transmit1}
-        list_of_commands[tx_start_time1] = transmission_command1
-        transmission_command2 = {outlink2: self.messages_to_transmit2}
-        list_of_commands[tx_start_time2] = transmission_command2
-        self.player.load_transmission_commands(list_of_commands)
-
-    def test_recorder1_gets_correct_message(self):
-        self.env.run(until=float("inf"))
-        received_messages1 = self.recorder1.recorded_messages
-        assert self.messages_to_transmit1 == received_messages1
-
-    def test_recorder2_gets_correct_message(self):
-        self.env.run(until=float("inf"))
-        received_messages2 = self.recorder2.recorded_messages
-        assert self.messages_to_transmit2 == received_messages2
+@pytest.fixture(params=[(1000, 13)])
+def link1(env, request):
+    config = request.param
+    new_link = make_link(config, env)
+    return new_link
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture(params=[(1000, 13)])
+def link2(env, request):
+    config = request.param
+    new_link = make_link(config, env)
+    return new_link
+
+
+@pytest.fixture
+def recorder1(env, link1):
+    from ft4fttsim.networking import MessageRecordingDevice
+    recorder = MessageRecordingDevice(env, "recorder1")
+    recorder.connect_inlink(link1)
+    return recorder
+
+
+@pytest.fixture
+def recorder2(env, link2):
+    from ft4fttsim.networking import MessageRecordingDevice
+    recorder = MessageRecordingDevice(env, "recorder2")
+    recorder.connect_inlink(link2)
+    return recorder
+
+
+@pytest.fixture
+def player(env, link1, link2):
+    from ft4fttsim.networking import MessagePlaybackDevice
+    player = MessagePlaybackDevice(env, "player")
+    player.connect_outlink(link1)
+    player.connect_outlink(link2)
+    return player
+
+
+@pytest.fixture
+def player_diff(env, player, recorder1, recorder2):
+    """
+    Set up player sending a separate message at a different instant of time
+    to each recorder.
+    """
+    from ft4fttsim.networking import Message
+    tx_start_time1, tx_start_time2 = range(2)
+    messages1 = [Message(env, player, recorder1, 543, "message for recorder1")]
+    messages2 = [Message(env, player, recorder2, 453, "message for recorder2")]
+    port1 = player.outlinks[0]
+    port2 = player.outlinks[1]
+    player.load_transmission_commands(
+        {
+            0: {port1: messages1},
+            1: {port2: messages2},
+        }
+    )
+    player.messages1 = messages1
+    player.messages2 = messages2
+    return player
+
+
+def test_recorder1_gets_correct_message(env, player_diff, recorder1):
+    env.run(until=float("inf"))
+    assert player_diff.messages1 == recorder1.recorded_messages
+
+
+def test_recorder2_gets_correct_message(env, player_diff, recorder2):
+    env.run(until=float("inf"))
+    assert player_diff.messages2 == recorder2.recorded_messages
+
+
+def test_recorder1_does_not_get_wrong_message(env, player_diff, recorder1):
+    env.run(until=float("inf"))
+    assert player_diff.messages2 != recorder1.recorded_messages
+
+
+def test_recorder2_does_not_get_wrong_message(env, player_diff, recorder2):
+    env.run(until=float("inf"))
+    assert player_diff.messages1 != recorder2.recorded_messages
