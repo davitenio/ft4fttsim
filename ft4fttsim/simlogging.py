f25267b50f01ab3afeb8a6d0f6ca7498652949e4
David Gessner <davidges@gmail.com>
2013-11-25 10:04:27 +0100

    simlogging.py: changes to silence pylint complaints

--- ft4fttsim/simlogging.py
+++ ft4fttsim/simlogging.py
@@ -38,19 +38,26 @@ import logging
 
 # Instance of simpy.Environment to use to timestamp logging entries.
 # Code using this module should set a value for this.
-env = None
+env = None  # pylint: disable-msg=C0103
 
 
 class SimLoggerAdapter(logging.LoggerAdapter):
+    """
+    Class used to prefix the simulated time to the logging entries.
+
+    """
+
     def process(self, log_msg, kwargs):
         if env is not None:
             return "{:>8.2f}: {}".format(env.now, log_msg), kwargs
         else:
             return "{}".format(log_msg), kwargs
 
+
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(levelname)5s:%(filename)15s:%(lineno)5d: %(message)s")
 
-logger = logging.getLogger('ft4fttsim')
-log = SimLoggerAdapter(logger, {})
+
+LOGGER = logging.getLogger('ft4fttsim')
+log = SimLoggerAdapter(LOGGER, {})  # pylint: disable-msg=C0103
