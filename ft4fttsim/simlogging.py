112ca5f973470ce3229f52312cab9bc206300ac4
David Gessner <davidges@gmail.com>
2013-12-07 12:56:17 +0100

    Revert "Add docstring for simlogging module"
    This reverts commit 4ba02ed4e0ce00b7454d2d65e15ea1659e734d32.
    
    The doctest introduced in the reverted commit prevents the logs from being
    printed when a test fails.

--- ft4fttsim/simlogging.py
+++ ft4fttsim/simlogging.py
@@ -1,37 +1,4 @@
 # author: David Gessner <davidges@gmail.com>
-"""
-This module allows to print log messages timestamped with the simulated time.
-
-The format of the log messages is the following:
-
-LOGLEVEL:FILENAME:LINENUMBER:SIMULATEDTIME:LOGMESSAGE
-
-Example:
-
->>> import simpy
->>> import ft4fttsim.simlogging
->>> import sys
->>> # The following line is to redirect stderr to stdout in this doctest.
->>> sys.stderr.write = sys.stdout.write
->>> env = simpy.Environment()
->>> # Register the simulation environment with the logger.
->>> ft4fttsim.simlogging.env = env
->>> def hello(env):
-...     ft4fttsim.simlogging.log.debug("hello started")
-...     print("Hello ", end="")
-...     yield env.timeout(1234)
-...     print("World!")
-...     ft4fttsim.simlogging.log.debug("hello finished")
-...
->>> hello_process = hello(env)
->>> env.process(hello_process)
-<Process(hello) object at 0x...>
->>> env.run(until=10000)
-DEBUG:<doctest ft4fttsim.simlogging[6]>:    2:     0.00: hello started
-Hello World!
-DEBUG:<doctest ft4fttsim.simlogging[6]>:    6:  1234.00: hello finished
-
-"""
 
 import logging
 
